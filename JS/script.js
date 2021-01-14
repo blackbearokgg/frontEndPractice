76093d648051ce416430bd813a451647f37c3ba5
blackbearokgg <blackbearzx@gmail.com>
2021-01-15 02:39:27 +0800

    show intro

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,11 @@
 // ===coolIntroAnimate==By==DevEd==
-// const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-// tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-// tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-// tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-// tl.fromTo("header", { opacity: 0 }, { opacity: 1, duration: 1 });
-// tl.fromTo(".nav-btn", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("header", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".nav-btn", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 
 // ========Jquery==NavBar=for=RWD========
 $(window).bind({
